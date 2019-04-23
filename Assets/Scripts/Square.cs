439aa7961b9c1656b23fc738b859c8c7efb59598
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2019-04-22 22:55:38 -0700

    square pseudo done

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Square : MonoBehaviour
 {
+	public List<Tuple<int, int>>presentMarks;
     // Start is called before the first frame update
     void Start()
     {
-        
+        presentMarks = new List<Tuple<int, int>>()
     }
 
+    bool addMark(int player, int turn)
+    {
+    	Tuple<int, int> mark = new Tuple<int, int>(player, turn);
+    	presentMarks.add(mark);
+    	return true;
+    }
     // Update is called once per frame
     void Update()
     {
