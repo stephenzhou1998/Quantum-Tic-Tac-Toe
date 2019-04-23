cbecb854688089eb9a18cba87c0492ffe27063c9
Stephen Zhou <stephenzhou@berkeley.edu>
2019-04-22 23:10:39 -0700

    add Bot class

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void getNextMove(Board board)
+    {
+        // Perform tree search for best strategy
+    }
+
+    private evalState(Board board)
+    {
+        // Create leaf node of the search tree
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
