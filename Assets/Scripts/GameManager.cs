b66f3f6e645072e94c61edfb1ff22db2126ebcaa
Stephen Zhou <stephenzhou@berkeley.edu>
2019-04-22 23:01:46 -0700

    merge

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int currentPlayer;
+    private int turnNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayer = 1;
+        turnNum = 1;
+    }
+
+    public int getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+
+    public int getTurnNum()
+    {
+        return turnNum;
+    }
+
+    public void nextTurn()
+    {
+        if (currentPlayer == 1)
+        {
+            currentPlayer = 2;
+        } else if (currentPlayer == 2)
+        {
+            currentPlayer = 1;
+        }
+        turnNum++;
+    }
+}
--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class Square : MonoBehaviour
 {
+    private Transform innerSquares;
+    private GameManager gameManager;
+    public GameObject X;
+    public GameObject O;
+    private int filledMarks;
+    private int chosenMarks;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        innerSquares = transform.Find("InnerSquares");
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    public void addMark()
+    {
+        GameObject toAdd;
+        if (gameManager.getCurrentPlayer() == 1)
+        {
+            toAdd = X;
+        } else if (gameManager.getCurrentPlayer() == 2) {
+            toAdd = O;
+        }
+        foreach (Transform square in innerSquares)
+        {
+
+        }
+    }
+
+    public void test()
+    {
+        return;
     }
 
     // Update is called once per frame
