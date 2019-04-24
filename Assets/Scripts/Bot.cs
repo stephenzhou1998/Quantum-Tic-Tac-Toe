8ce9c6c789dbdefd321a2dca4610fee9713bce27
Ruixin Huang <ruixinhuang@MacBook-Pro-202.local>
2019-04-24 00:28:26 -0700

    bot.cs

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -12,8 +12,46 @@ public class Bot : MonoBehaviour
         
     }
 
-    public void getNextMove(Board board)
-    {
+
+    public Action getNextMove(Board board,int actionType,int difficulty)
+    {   
+        List<Action> legalmoves = getallmoves(board,1); // 1 means bot
+        List<int> Scores = new List<int>();
+
+        
+        for(Action i:legalmoves){
+            if(i.actionType == actionType){
+                Board copy = Board.copy(board);
+                Scores.add(evalMove(i,copy,1));
+            }
+        }
+        int max = max(Scores);
+        int index = Scores.indexof(max);
+        return legalmoves[index];
+
+
+        
+        // Perform tree search for best strategy
+    }
+
+    public Action getNextMoveopponent(Board board,int actionType,int difficulty)
+    {   
+        List<Action> legalmoves = getallmoves(board,0); // 0 means opponent
+        List<int> Scores = new List<int>();
+
+        
+        for(Action i:legalmoves){
+            if(i.actionType == actionType){
+                Board copy = Board.copy(board);
+                Scores.add(evalMove(i,copy,0,difficulty));
+            }
+        }
+        int min = min(Scores);
+        int index = Scores.indexof(min);
+        return legalmoves[index];
+
+
+        
         // Perform tree search for best strategy
     }
 
@@ -21,6 +59,26 @@ public class Bot : MonoBehaviour
     {
         // Create leaf node of the search tree
     }
+    
+
+    public int evalMove(Action move,Board board,int agent,int difficulty){
+        double score;
+        if(difficulty == 0){
+            return ;
+        }
+        if(agent == 1){
+            getNextMoveopponent(Board board,int actionType,int difficulty)
+
+        }
+
+    }
+
+    public List<Action> getallmoves(Board board,int agent){
+
+
+
+    }
+    */
 
     // Update is called once per frame
     void Update()
