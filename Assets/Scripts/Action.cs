38d4c9f3f8913f9e250b4ff8dafbe21d717c0f48
Pairode Jaroensri <p.k.jaroensri@berkeley.edu>
2019-04-23 23:36:07 -0700

    Action class done

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Action : MonoBehaviour
 {
+    // 0 means placing new spookyMarks, 1 means choosing square to collapse
+    int actionType;
+    SpookyMark spMark;
+    Square sq;
+
+    public Action(SpookyMark markToAdd)
+    {
+        actionType = 0;
+        spMark = markToAdd;
+        sq = null;
+    }
+
+    public Action(Square toCollapse)
+    {
+        actionType = 1;
+        sq = toCollapse;
+        spMark = null;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
